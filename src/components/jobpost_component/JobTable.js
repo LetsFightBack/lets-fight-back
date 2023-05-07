7c27d3664936b16c31ecb2f786394c6f3a5fcbb2
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2023-05-07 22:50:36 +0530

    Filter menu option updated. Sort the Table by date

--- src/components/jobpost_component/JobTable.js
+++ src/components/jobpost_component/JobTable.js
@@ -13,98 +13,99 @@ const JobTables = () => {
 
     const [filterInput, setFilterInput] = useState('');
     const [filterColumn, setFilterColumn] = useState('');
-    // const [DATA, setDATA] = useState([{
-    //     "role": "Graduate Engineer Trainee ",
-    //     "batch": [
-    //         "2023",
-    //         "2024",
-    //         "2025"
-    //     ],
-    //     "applyLink": "https://bit.ly/41zWXls",
-    //     "timestamp": {
-    //         "seconds": 1683297353,
-    //         "nanoseconds": 576000000
-    //     },
-    //     "jobType": "Internship",
-    //     "location": "Gurgaon",
-    //     "postingDate": "05-06-2024",
-    //     "companyName": "Siemens Energy "
-    // },
-    // {
-    //     "companyName": "Faurecia",
-    //     "postingDate": "06-06-2024",
-    //     "applyLink": "https://faurecia.eightfold.ai/careers/job/563087396050746?domain=faurecia.com",
-    //     "role": "Analyst - PSP",
-    //     "batch": [
-    //         "2023"
-    //     ],
-    //     "location": "Pune",
-    //     "timestamp": {
-    //         "seconds": 1683311931,
-    //         "nanoseconds": 585000000
-    //     },
-    //     "jobType": "Internship"
-    // },
-    // {
-    //     "applyLink": "Associate Consultant",
-    //     "role": "Associate Consultant",
-    //     "postingDate": "06-06-2024",
-    //     "companyName": " KPMG International",
-    //     "timestamp": {
-    //         "seconds": 1683397445,
-    //         "nanoseconds": 478000000
-    //     },
-    //     "location": "Pune",
-    //     "batch": [
-    //         "2022",
-    //         "2023",
-    //         "2024",
-    //         "2025"
-    //     ],
-    //     "jobType": "Full Time"
-    // },
-    // {
-    //     "companyName": "Share Chat",
-    //     "applyLink": "https://sharechat.mynexthire.com/employer/jobs?src=careers&p=eyJwYWdlVHlwZSI6ImpkIiwiY3ZTb3VyY2UiOiJjYXJlZXJzIiwicmVxSWQiOjE0ODAsInJlcXVlc3RlciI6eyJpZCI6IiIsImNvZGUiOiIiLCJuYW1lIjoiIn0sInBhZ2UiOiJjYXJlZXJzIiwiYnVmaWx0ZXIiOi0xLCJjdXN0b21GaWVsZHMiOnt9fQ==",
-    //     "location": "Bangalore",
-    //     "role": "Software Developer-1 (React Native)",
-    //     "postingDate": "04-06-2024",
-    //     "batch": [
-    //         "2020",
-    //         "2021",
-    //         "2022"
-    //     ],
-    //     "jobType": "Full Time",
-    //     "timestamp": {
-    //         "seconds": 1683230147,
-    //         "nanoseconds": 162000000
-    //     }
-    // }
-
-    // ])
-    const [DATA, setDATA] = useState([])
-    const jobs = async () => {
-        const s = await getJobs()
-        setDATA(s.map((job) => {
-            return {
-                companyName: job?.companyName,
-                role: job?.role,
-                location: job?.location,
-                jobType: job?.jobType,
-                postingDate: job?.postingDate,
-                batch: job?.batch,
-                applyLink: job?.applyLink
-            }
-        }))
+    const [DATA, setDATA] = useState([{
+        "role": "Graduate Engineer Trainee ",
+        "batch": [
+            "2023",
+            "2024",
+            "2025"
+        ],
+        "applyLink": "https://bit.ly/41zWXls",
+        "timestamp": {
+            "seconds": 1683297353,
+            "nanoseconds": 576000000
+        },
+        "jobType": "Internship",
+        "location": "Gurgaon",
+        "postingDate": "2023-05-05",
+        "companyName": "Siemens Energy "
+    },
+    {
+        "companyName": "Faurecia",
+        "postingDate": "2023-05-06",
+        "applyLink": "https://faurecia.eightfold.ai/careers/job/563087396050746?domain=faurecia.com",
+        "role": "Analyst - PSP",
+        "batch": [
+            "2023"
+        ],
+        "location": "Pune",
+        "timestamp": {
+            "seconds": 1683311931,
+            "nanoseconds": 585000000
+        },
+        "jobType": "Internship"
+    },
+    {
+        "applyLink": "Associate Consultant",
+        "role": "Associate Consultant",
+        "postingDate": "2023-05-06",
+        "companyName": " KPMG International",
+        "timestamp": {
+            "seconds": 1683397445,
+            "nanoseconds": 478000000
+        },
+        "location": "Pune",
+        "batch": [
+            "2022",
+            "2023",
+            "2024",
+            "2025"
+        ],
+        "jobType": "Full Time"
+    },
+    {
+        "companyName": "Share Chat",
+        "applyLink": "https://sharechat.mynexthire.com/employer/jobs?src=careers&p=eyJwYWdlVHlwZSI6ImpkIiwiY3ZTb3VyY2UiOiJjYXJlZXJzIiwicmVxSWQiOjE0ODAsInJlcXVlc3RlciI6eyJpZCI6IiIsImNvZGUiOiIiLCJuYW1lIjoiIn0sInBhZ2UiOiJjYXJlZXJzIiwiYnVmaWx0ZXIiOi0xLCJjdXN0b21GaWVsZHMiOnt9fQ==",
+        "location": "Bangalore",
+        "role": "Software Developer-1 (React Native)",
+        "postingDate": "2023-05-05",
+        "batch": [
+            "2020",
+            "2021",
+            "2022"
+        ],
+        "jobType": "Full Time",
+        "timestamp": {
+            "seconds": 1683230147,
+            "nanoseconds": 162000000
+        }
     }
-    useEffect(() => {
-        jobs();
+    
+    ])
+    // const [DATA, setDATA] = useState([])
+    // const jobs = async () => {
+    //     const s = await getJobs()
+    //     setDATA(s.map((job) => {
+    //         return {
+    //             companyName: job?.companyName,
+    //             role: job?.role,
+    //             location: job?.location,
+    //             jobType: job?.jobType,
+    //             postingDate: job?.postingDate,
+    //             batch: job?.batch,
+    //             applyLink: job?.applyLink
+    //         }
+    //     }))
+    // }
+    // useEffect(() => {
+    //     jobs();
 
-    }, [])
+    // }, [])
     const columns = useMemo(() =>
         COLUMNS, [COLUMNS])
     const data = useMemo(() =>
         DATA, [DATA])
+
 
     const filterRows = useMemo(() => {
         // console.log(filterColumn, filterInput);
@@ -116,27 +117,29 @@ const JobTables = () => {
 
     }, [filterColumn, filterInput, data])
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, } = useTable({
-        columns, data: filterRows, initialState: {
+        columns, data: filterRows,
+        defaultColumn: { Filter: () => null },
+        initialState: {
             sortBy: [
                 {
-                    id: 'postingDate', desc: true
+                    id: 6, desc: true
                 }
             ]
         },
-        defaultColumn: { Filter: () => null },
     },
         useFilters,
-        useSortBy)
+        useSortBy
+    )
 
     return (
         <>
             {
                 <>
                     <div className='filterJobBar' >
-                    <select value={filterColumn} onChange={e => setFilterColumn(e.target.value)}>
+                        <select value={filterColumn} onChange={e => setFilterColumn(e.target.value)}>
                             <option value="">Select Filter</option>
                             {COLUMNS.map(column => (
-                                column.accessor !== 'year_of_experience' && column.accessor !== 'batch' && column.accessor !== 'apply' && column.accessor !== 'date_of_posting' &&
+                                column.accessor !== 'year_of_experience' && column.accessor !== 'location' && column.accessor !== 'batch' && column.accessor !== 'applyLink' && column.accessor !== 'postingDate' &&
                                 <option key={column.accessor} value={column.accessor}>
                                     {column.Header}
                                 </option>
@@ -147,7 +150,7 @@ const JobTables = () => {
                             <input type="text" id="filterInput" value={filterInput} onChange={e => {
                                 return (setFilterInput(e.target.value))
                             }} />
-                        </div> 
+                        </div>
                     </div>
                     <table {...getTableProps()} style={{ width: "100%", marginBottom: "2rem" }}>
                         <thead>
@@ -185,21 +188,23 @@ const JobTables = () => {
                                         {row.cells.map(cell => {
                                             if (cell.column.id === 7)
                                                 return (
-                                                    <a href={cell.value} target='_blank'>
-                                                        <td {...cell.getCellProps({ style: cell.column.style })}>
+                                                    <td {...cell.getCellProps({ style: cell.column.style })}>
+                                                        <a href={cell.value} target='_blank' style={{ textDecoration: "none", color: "white" }}>
                                                             {/* <button> */}
                                                             Apply
                                                             <ArrowOutwardIcon sx={{ marginLeft: "7px", fontSize: "12px" }} />
                                                             {/* </button> */}
-                                                        </td>
-                                                    </a>
+                                                        </a>
+                                                    </td>
                                                 )
+                                            console.log(cell.column);
                                             if (cell.column.id === 5)
                                                 return (
                                                     <td {...cell.getCellProps({ style: cell.column.style })}>
                                                         {cell.value.join(', ')}
                                                     </td>
                                                 )
+
                                             return (
                                                 <td
                                                     {...cell.getCellProps({ style: cell.column.style })}
@@ -213,6 +218,7 @@ const JobTables = () => {
                                                 //     fontWeight: "300"
                                                 // }}
                                                 >
+
                                                     {/* { cell.render('Cell')} */}
                                                     {cell.value === undefined || cell.value === "" || cell.value === null ? "NA" : cell.render('Cell')}
                                                 </td>
